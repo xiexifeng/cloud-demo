ef3c5fbbbb448ccdf041b6fda137af1c3d177800
xifengbin <xifeng_bin@qq.com>
2019-01-31 10:29:24 +0800

    init 站位

---
+++ cloud-feign/src/main/java/com/xifeng/feign/FeignApplication.java
@@ -0,0 +1,10 @@
+package com.xifeng.feign;
+
+public class FeignApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ cloud-hystrix/src/test/java/com/xifeng/hystrix/test/Test.java
@@ -0,0 +1,5 @@
+package com.xifeng.hystrix.test;
+
+public class Test {
+
+}
